2ee4b5cff208a0a6ab6ebf0ded5cc7aae421f669
kanaevbaytik <118180389+kanaevbaytik@users.noreply.github.com>
2025-07-21 22:00:34 +0600

    Initial Commit

---
+++ ImageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageApp
+//
+//  Created by Baytik  on 21/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
